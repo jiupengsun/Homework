ae14ff9117e8060dc52584a1dfb4433e4ac9b478
samparly <samparly@gmail.com>
2016-05-17 01:31:38 +0800

    Homework

--- chapter2/hello.c
+++ chapter2/hello.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "byte.h"
 
-typedef struct{
-	int a[2];
-	double d;
-} struct_t;
 
-double fun(int i){
-	volatile struct_t s;
-	s.d = 3.14;
-	s.a[i] = 1073741824;
-	return s.d;
+unsigned float_i2f(int x) {
+	int sign = 0;
+	int E = 0;
+	int frac = 0;
+	unsigned ux;
+	int hb = 31;
+	sign = (x >> 31) & 0x1;
+	if(sign)
+		x = -x;
+	ux = x;
+	while(!(ux >> hb) && hb)
+		--hb;// get the highest significant bit
+	if(hb > 23){// need rounding
+		int monitor = 1 << (hb - 24);
+		int guard = monitor << 1;
+		int round = (guard - 1) & ux;
+		if((round > monitor) || ((round==monitor) && (guard & ux))){
+			ux += guard;
+			if((1<<hb) & ux)
+				++hb;// carry bit
+		}
+		ux >>= hb - 23;
+	} else
+		ux <<= 23 - hb;
+	frac = ux & 0x7fffff;
+	if(x)
+		E = hb + 127;
+	return (sign << 31) | (E << 23) | frac;
 }
 
 int main(int argc, char* argv[]){
-	printf("%lf\n", fun(0));
-	printf("%lf\n", fun(1));
-	printf("%lf\n", fun(2));
-	printf("%lf\n", fun(3));
-	printf("%lf\n", fun(4));
-	printf("%lf\n", fun(6));
+	int x = 0x80000001;
+	show_int(float_i2f(x));
 	return 0;
 }
