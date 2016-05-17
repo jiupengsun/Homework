ddfca5015b14e666ca372ed09fefe5e6a7a9d0c9
samparly <samparly@gmail.com>
2016-05-18 01:16:52 +0800

    Homework

--- chapter3/byte.h
+++ chapter3/byte.h
@@ -4,7 +4,7 @@ typedef unsigned char * byte_pointer;
 void show_bytes(byte_pointer start, int len){
 	int i;
 	for(int i=0; i<len; ++i)
-		printf("%.2x", start[i]);
+		printf("%.2x ", start[i]);
 	printf("\n");
 }
 
--- chapter3/struct.c
+++ chapter3/struct.c
@@ -4,18 +4,24 @@
 struct S1 {
 	int i;
 	char c;
+	char d;
 	int j;
 };
 
+struct P4 {
+	short w[3];
+	char *c[3];
+};
+
 int main(){
-	struct S1 ss;
-	struct S1 *sp = &ss;
-	ss.i=1;
-	ss.c = 'a';
-	ss.j = 2;
-	printf("%d\n", *(sp));
-	printf("%d\n", sizeof(ss));
-	show_bytes((byte_pointer)&ss, sizeof(struct S1));
+	struct P4 p;
+	struct P4 *sp = &p;
+	p.w[0] = 1;
+	p.w[1] = 2;
+	p.w[2] = 3;
+	p.c[0] = "test";
+	printf("%d\n", sizeof(p));
+	show_bytes((byte_pointer)&p, sizeof(struct P4));
 //	printf("%c\n", (char*)(sp+4));
 //	printf("%d\n", *(sp));
 	return 0;
