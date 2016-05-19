340aac89daa02e9e3b6cc9031e782a7f97f8d02e
samparly <samparly@gmail.com>
2016-05-20 01:56:00 +0800

    Homework

--- chapter2/hello.c
+++ chapter2/hello.c
@@ -4,42 +4,50 @@
 #include "byte.h"
 
 
-unsigned float_i2f(int x) {
+unsigned float_i2f(int x){
 	int sign;
-	unsigned u;
-	unsigned ux = x;
-	int hb = 0;
-	int diff;
+	int count = 0;
+	int R;
+	int G;
+	int S;
 	if(x){
+		// get the sign of x
 		sign = x & 0x80000000;
 		if(sign)
-			ux = -x;
-		// get the highest significant bit
-		u = ux;
-		while(u){
-			u >>= 1;
-			++hb;
+			// x is negative
+			// get the abs of x
+			x = -x;
+		while(x > 0){
+			// move the highest bit to the left most
+			x <<= 1;	
+			++count;
 		}
-		diff = hb - 23;
-		if(diff > 0){// need rounding
-			int guard = 1 << diff;
-			int monitor = guard >> 1;
-			int round = (guard - 1) & ux;
-			if((round > monitor) || ((round==monitor) && (guard & ux))){
-				ux += guard;
-				if(!((1<<hb) & ux))
-					++hb;// carry bit
+		R = 0x100 & x;
+		G = 0x80 & x;
+		S = 0x7f & x;
+		if(G){
+			// only check rounding when G is larger than 0
+			// if S is not zero, then round to upper
+			// else if R is 1, then round to even
+			if(S | R){
+				//carry bit
+				x += R;	
+				//check whether overflow, if we already know the lsb of x is 1
+				//so if x is overflow, then the lsb of x is 0
+				count += !(x >> 31);
 			}
-			ux >>= diff;
-		} else
-			ux <<= -diff;
-		return sign | ((hb + 127) << 23) | (ux & 0x7fffff);
+		}
+		// we know the moving step is count, and we omit the highest position of 1
+		// so the total exponent is 32 - (count + 1), adding the bias
+		// so E is 32-(count+1)+127=158-count
+		return sign | ((158 - count) << 23) | ((x >> 8) & 0x7fffff);
 	}
+	// if x is 0, then the float is also 0
 	return 0;
 }
 
 int main(int argc, char* argv[]){
-	int x = 0x80000000;
+	int x = 0x80000001;
 	show_int(float_i2f(x));
 	return 0;
 }
