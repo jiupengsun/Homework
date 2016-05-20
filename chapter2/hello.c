21474b8deb85b4750de0921191c1705fd507d9c1
samparly <samparly@gmail.com>
2016-05-20 12:55:37 +0800

    Homework

--- chapter2/hello.c
+++ chapter2/hello.c
@@ -4,50 +4,22 @@
 #include "byte.h"
 
 
-unsigned float_i2f(int x){
-	int sign;
-	int count = 0;
-	int R;
-	int G;
-	int S;
-	if(x){
-		// get the sign of x
-		sign = x & 0x80000000;
-		if(sign)
-			// x is negative
-			// get the abs of x
-			x = -x;
-		while(x > 0){
-			// move the highest bit to the left most
-			x <<= 1;	
-			++count;
-		}
-		R = 0x100 & x;
-		G = 0x80 & x;
-		S = 0x7f & x;
-		if(G){
-			// only check rounding when G is larger than 0
-			// if S is not zero, then round to upper
-			// else if R is 1, then round to even
-			if(S | R){
-				//carry bit
-				x += R;	
-				//check whether overflow, if we already know the lsb of x is 1
-				//so if x is overflow, then the lsb of x is 0
-				count += !(x >> 31);
-			}
-		}
-		// we know the moving step is count, and we omit the highest position of 1
-		// so the total exponent is 32 - (count + 1), adding the bias
-		// so E is 32-(count+1)+127=158-count
-		return sign | ((158 - count) << 23) | ((x >> 8) & 0x7fffff);
-	}
-	// if x is 0, then the float is also 0
-	return 0;
+int rotateRight(int x, int n) {
+	/*
+	 * Retrieve n bit
+	 * Left shift x till 32 - n 
+	 * Or the n bit
+	 */
+	// x >=0 mask=0x0
+	// x <0 mask=0xffffffff
+	int mask = x >> 31;
+	// m=32-n
+	int m = 33 + ~n;
+	return ((mask ^ x) << m) ^ (x >> n);
 }
 
 int main(int argc, char* argv[]){
-	int x = 0x80000001;
-	show_int(float_i2f(x));
+	int x = 0x80000000;
+	show_int(rotateRight(x, 0));
 	return 0;
 }
