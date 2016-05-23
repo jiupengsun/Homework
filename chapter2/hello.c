a9cd52c0802aebeb500fc70c6c6c10bc9173339d
samparly <samparly@gmail.com>
2016-05-24 00:53:46 +0800

    Homework

--- chapter2/hello.c
+++ chapter2/hello.c
@@ -4,22 +4,14 @@
 #include "byte.h"
 
 
-int rotateRight(int x, int n) {
-	/*
-	 * Retrieve n bit
-	 * Left shift x till 32 - n 
-	 * Or the n bit
-	 */
-	// x >=0 mask=0x0
-	// x <0 mask=0xffffffff
-	int mask = x >> 31;
-	// m=32-n
-	int m = 33 + ~n;
-	return ((mask ^ x) << m) ^ (x >> n);
-}
-
 int main(int argc, char* argv[]){
-	int x = 0x80000000;
-	show_int(rotateRight(x, 0));
+	float a = 129;
+	float b = 256;
+	float c = a - b;
+	float d = -127;
+	show_float(a);
+	show_float(b);
+	show_float(c);
+	show_float(d);
 	return 0;
 }
